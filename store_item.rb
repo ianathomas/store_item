377ec1f1c8a8e40face29bcd13c0bef16af14d35
IAN THOMAS <ianthomas9891@gmail.com>
2023-05-02 16:52:57 -0400

    Changed the entire thing into a class with readers and a writer

--- store_item.rb
+++ store_item.rb
@@ -1,5 +1,32 @@
-item1 = {item_name: "fancyshirt", item_color: "red", item_source: "Cuba"}
-item2 = {item_name: "fancyshoes", item_color: "blue", item_source: "United States"}
-item3 = {item_name: "fancycar", item_color: "grey", item_source: "Italy"}
+class Store
+  def initialize(item_name, item_color, item_source)
+    @item_name = item_name
+    @item_color = item_color
+    @item_source = item_source
+  end
 
-p item3 {:itemname, :item_color, :item_source}
+  def item_name
+    @item_name
+  end
+
+  def item_color
+    @item_color
+  end
+
+  def item_source
+    @item_source
+  end
+
+  def item_source=(location)
+    @item_source = location
+  end
+end
+  
+item1 =Store.new("fancyshirt", "red", "Cuba")
+item2 =Store.new("fancyshoes", "blue", "United States")
+item3 =Store.new("fancycar", "grey", "Italy")
+p item1
+p item1.item_name
+p item2.item_color
+p item3.item_source
+p item3.item_source = "Phillipines"
