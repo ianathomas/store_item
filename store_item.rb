d0d615d2721b09ff2d5a00142470b550454221ec
IAN THOMAS <ianthomas9891@gmail.com>
2023-05-02 15:18:42 -0400

    made the whole thing without pushes, whoopsie

---
+++ store_item.rb
@@ -0,0 +1,5 @@
+item1 = {item_name: "fancyshirt", item_color: "red", item_source: "Cuba"}
+item2 = {item_name: "fancyshoes", item_color: "blue", item_source: "United States"}
+item3 = {item_name: "fancycar", item_color: "grey", item_source: "Italy"}
+
+p item3 {:itemname, :item_color, :item_source}
