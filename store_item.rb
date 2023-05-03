88593619862ebf54723705256bef98f7e02bfe29
IAN THOMAS <ianthomas9891@gmail.com>
2023-05-03 12:31:16 -0400

    changed variable to not rely on order for initialize

--- store_item.rb
+++ store_item.rb
@@ -1,30 +1,34 @@
 class Store
-  def initialize(item_name, item_color, item_source)
-    @item_name = item_name
-    @item_color = item_color
-    @item_source = item_source
+  attr_reader :item_source, :item_color, :item_name
+  attr_writer :item_source
+
+
+  def initialize(item_stuff)
+    @item_name = item_stuff[:item_name]
+    @item_color = item_stuff[:item_color]
+    @item_source = item_stuff[:item_source]
   end
 
-  def item_name
-    @item_name
-  end
+  # def item_name
+  #   @item_name
+  # end
 
-  def item_color
-    @item_color
-  end
+  # def item_color
+  #   @item_color
+  # end
 
-  def item_source
-    @item_source
-  end
+  # def item_source
+  #   @item_source
+  # end
 
-  def item_source=(location)
-    @item_source = location
-  end
+  # def item_source=(location)
+  #   @item_source = location
+  # end
 end
   
-item1 =Store.new("fancyshirt", "red", "Cuba")
-item2 =Store.new("fancyshoes", "blue", "United States")
-item3 =Store.new("fancycar", "grey", "Italy")
+item1 =Store.new(item_name: "fancyshirt", item_color: "red", item_source: "Cuba")
+item2 =Store.new(item_name: "fancyshoes", item_color: "blue", item_source: "United States")
+item3 =Store.new(item_name: "fancycar", item_color: "grey", item_source: "Italy")
 p item1
 p item1.item_name
 p item2.item_color
